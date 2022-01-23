b3a7ed2ca216d6273444872b092b7e96637ca1b8
ebbeckett <eric.beckett@uvm.edu>
2022-01-23 14:37:49 -0500

    with sleep

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
+from time import sleep
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1)
+    print(i)
+
 p.disconnect()
