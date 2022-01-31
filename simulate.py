29fe7ba83729db6354697404c78ac185e681a64e
ebbeckett <eric.beckett@uvm.edu>
2022-01-30 19:23:40 -0500

    added floor+gravity

--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
 from time import sleep
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8) #is this working?
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 for i in range(1000):
