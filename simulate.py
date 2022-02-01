90d39178691e384eeb52e81c672a0affaff16421
ebbeckett <eric.beckett@uvm.edu>
2022-01-31 22:18:54 -0500

    finished

--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8) #is this working?
-planeId = p.loadURDF("plane.urdf")
+planeId = p.loadURDF("plane.urdf")      
 robotId = p.loadURDF("body.urdf")   
 p.loadSDF("world.sdf")
 
