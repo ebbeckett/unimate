68d6a2ba7ff1081e0c4a38a4c2329d899dc7ea4c
ebbeckett <eric.beckett@uvm.edu>
2022-02-07 16:43:39 -0500

    updateded sensors

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ z= 1.5
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[3, 3, 0], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[3, 3, 0.5], size=[length, width, height])
     pyrosim.End()
 
 def Create_Robot():
@@ -20,6 +20,7 @@ def Create_Robot():
     torso_frontleg_pos = [x+(0.5*length),y,z-(height/2)]
 
     pyrosim.Start_URDF("body.urdf")
+
     pyrosim.Send_Cube(name="Torso", pos=[x, y, 1.5], size=[length, width, height])
     pyrosim.Send_Joint(name = "Torso_Backleg", parent= "Torso", child = "Backleg", type = "revolute", position = torso_backleg_pos)
     pyrosim.Send_Cube(name="Backleg", pos=backleg_pos, size=[length, width, height])
--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import os
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,16 +14,19 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 #0.017
+
 pyrosim.Prepare_To_Simulate(robotId)
-#backLegSensorValues = numpy.zeros(10000)
+backLegSensorValues = numpy.zeros(100)
 
-
-for i in range(10000):
-    time.sleep(0.017)
+for i in range(100):
     p.stepSimulation()
-    #backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+    time.sleep(0.017)
     #print(i)
+    #print(backLegSensorValues[i])
 
+numpy.save(os.path.join('data','bcklegvalfile'), backLegSensorValues)
+
+print(backLegSensorValues)
 p.disconnect()
 
-#print(backLegSensorValues)
