652b7de84f24d5046802d6871783369c0683da7b
ebbeckett <eric.beckett@uvm.edu>
2022-02-13 23:27:00 -0500

    new changes

--- analyze.py
+++ analyze.py
@@ -1,14 +1,19 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load('data/bcklegvalfile.npy')
-print(backLegSensorValues)
+#backLegSensorValues = numpy.load('data/bcklegvalfile.npy')
+#print(backLegSensorValues)
 
-frntLegSensorValues = numpy.load('data/frntlegvalfile.npy')
-print(backLegSensorValues)
+#frntLegSensorValues = numpy.load('data/frntlegvalfile.npy')
+#print(backLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, linewidth=2, label="Backleg Sensor Vals")
-matplotlib.pyplot.plot(frntLegSensorValues, linewidth=2, label="Frontleg Sensor Vals")
+#matplotlib.pyplot.plot(backLegSensorValues, linewidth=2, label="Backleg Sensor Vals")
+#matplotlib.pyplot.plot(frntLegSensorValues, linewidth=2, label="Frontleg Sensor Vals")
+#matplotlib.pyplot.legend()
+
+sinVal = numpy.load('data/numpsin.npy')
+
+matplotlib.pyplot.plot(sinVal,linewidth=2, label="sin val" )
 matplotlib.pyplot.legend()
 
 matplotlib.pyplot.show()
---
+++ testing.py
@@ -0,0 +1,8 @@
+import numpy 
+import random
+import os
+
+target_angles = x = numpy.linspace(0, numpy.pi, 100)
+print(target_angles)
+
+
