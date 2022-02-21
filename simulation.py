7abc16d40056655b211bb92d7853df04276ad86f
ebbeckett <eric.beckett@uvm.edu>
2022-02-21 12:53:06 -0500

    smh

--- simulation.py
+++ simulation.py
@@ -9,10 +9,12 @@ class SIMULATION:
         self.physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8) #is this working?
+        self.planeId = p.loadURDF("plane.urdf")
+        p.loadSDF("world.sdf")
         self.world = WORLD()
         self.robot = ROBOT()
         
     
-        pyrosim.Prepare_To_Simulate(ROBOT.robotID) # may be an error
+        pyrosim.Prepare_To_Simulate(p.loadURDF("body.urdf")) # may be an error
 
 
