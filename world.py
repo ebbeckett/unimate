5e27c555d4138558a0b0520fe628e96d967ca311
ebbeckett <eric.beckett@uvm.edu>
2022-02-21 12:53:01 -0500

    smh

--- world.py
+++ world.py
@@ -5,6 +5,4 @@ import pybullet_data
 class WORLD:
     def __init__(self):
         self.planeId = p.loadURDF("plane.urdf")
-        p.loadSDF("world.sdf")
-         
-        
+        p.loadSDF("world.sdf") 
