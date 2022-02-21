27f463a6ecccdbc28fc1199f6fa3703ec3d03e9e
ebbeckett <eric.beckett@uvm.edu>
2022-02-21 12:09:44 -0500

    doesn't open

--- simulation.py
+++ simulation.py
@@ -1,3 +1,15 @@
+from world import WORLD
+from robot import ROBOT
+import pybullet as p
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+
 class SIMULATION:
     def __init__(self):
-        self.world = WORLD() 
+        self.world = WORLD()
+        self.robot = ROBOT()
+
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8) #is this working?   
+        pyrosim.Prepare_To_Simulate(robotID)
