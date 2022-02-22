b93554d29c490d62e09aa87876563a7e5a8963ce
ebbeckett <eric.beckett@uvm.edu>
2022-02-22 17:47:54 -0500

    new update bus

--- robot.py
+++ robot.py
@@ -1,8 +1,21 @@
+from re import S
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
+from sensor import SENSOR
 
 class ROBOT:
     def __init__(self):
-        self.sensors = {}
         self.motors = {}
         #self.robotId = p.loadURDF("body.urdf")   
+    
+    def Prepare_To_Sense(self):
+        self.sensors = {}
+        for linkName in pyrosim.linkNamesToIndices: # is this for loop here or simulation?
+            self.sensors[linkName] = SENSOR(linkName)
+    
+    def Sense(self):
+        #frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
+        for key in self.sensors:
+            SENSOR.Get_Value(self) # this may be wrong
+            print(key)
+        
