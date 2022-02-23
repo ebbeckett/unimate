20415149ea86b2d168b82e16bc356c13c0df29d5
ebbeckett <eric.beckett@uvm.edu>
2022-02-22 19:57:03 -0500

    working now/step 74

--- robot.py
+++ robot.py
@@ -5,6 +5,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 
+# changed to jointname and jointnamestoindicies rather than link name, causing error as of now 
 class ROBOT:
     def __init__(self):
         #self.robotId = p.loadURDF("body.urdf") 
@@ -12,12 +13,14 @@ class ROBOT:
     
     def Prepare_To_Sense(self):
         self.sensors = {}
-        for jointName in pyrosim.jointNamesToIndices:
-            self.sensors[jointName] = SENSOR(jointName)
+        for linkName in pyrosim.linkNamesToIndices:
+            self.sensors[linkName] = SENSOR(linkName)
     
     def Sense(self, i):
         for key in self.sensors:
             self.sensors[key].Get_Value(i)
     
     def Prepare_To_Act(self):
-        pass
+        self.joints = {}
+        for jointName in pyrosim.jointNamesToIndices:
+            self.sensors[jointName] = SENSOR(jointName)
