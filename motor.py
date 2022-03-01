6ddf40db7cae7d7a0ecfbca108c02d47415c5866
ebbeckett <eric.beckett@uvm.edu>
2022-02-28 20:38:42 -0500

    neuorns finished

--- motor.py
+++ motor.py
@@ -18,20 +18,7 @@ class MOTOR:
 
         self.targetAngles = np.linspace(-np.pi, 2*np.pi, 1000)
 
-        MOTOR.Prepare_To_Act(self)
-        #self.Prepare_To_Act(c.bck_amplitude, c.bck_frequency, c.bck_phaseOffset)
     
-    def Prepare_To_Act(self):
-        if(self.jointName == "Torso_Frontleg"):     
-            for x in range(len(self.targetAngles)):
-                self.motorValues.append(self.amplitude * np.sin((self.frequency * x) + self.offset))
-        else:
-            for x in range(len(self.targetAngles)):
-                self.motorValues.append(self.amplitude/.5 * np.sin((self.frequency * x)/0.5 + self.offset))
-
-
     def Set_Value(self, desiredAngle, robot):
         pyrosim.Set_Motor_For_Joint(robot, self.jointName, p.POSITION_CONTROL, desiredAngle, 20)
     
-    def Save_Values(self):
-        np.save("data/" + str(self.jointName) + "Joint", self.values)
--- robot.py
+++ robot.py
@@ -33,17 +33,14 @@ class ROBOT:
     
     def Act(self, i):
         for neuronName in self.nn.Get_Neuron_Names():
-            #self.motors(neuronName).Set_Value(i, self.robotId)
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
 
                 self.motors[jointName].Set_Value(desiredAngle, self.robotId)
-                print("Neuron Name:", neuronName, " Joint Name:", jointName, " Desired Angle:", desiredAngle)
+                #print("Neuron Name:", neuronName, " Joint Name:", jointName, " Desired Angle:", desiredAngle)
 
 
-        # for key in self.motors:
-        #     self.motors[key].Set_Value(i, self.robotId)
 
     def Think(self):
         self.nn.Update()
