26c7d9966cb13890d93dc8793b25c936931ad653
ebbeckett <eric.beckett@uvm.edu>
2022-03-14 19:30:31 -0400

    initial

--- pyrosim/search.py
+++ pyrosim/search.py
@@ -1,5 +1,8 @@
 import os
+import hillclimber.py as HILL_CLIMBER
 
+hc = HILL_CLIMBER
 for i in range(5):
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+    # os.system("python3 generate.py")
+    # os.system("python3 simulate.py")
+    os.system("python3 hillclimber.py")
