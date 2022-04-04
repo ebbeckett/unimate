788db337bda443ce0e8cd4a33f34f8994019faef
ebbeckett <eric.beckett@uvm.edu>
2022-04-04 15:19:16 -0400

    final

--- search.py
+++ search.py
@@ -3,11 +3,6 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
 import hillclimber as hc
 
-# class SEARCH:
-#     def __init__(self):
-#         print('********* Got here in search.py *********')
-#         self.hc.Evolve()
-
 os.system("python3 parallelHillClimber.py")
 
 phc = PARALLEL_HILL_CLIMBER()
