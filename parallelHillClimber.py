c510f585a57b5752d31481deed5ccf781f464024
ebbeckett <eric.beckett@uvm.edu>
2022-04-25 18:46:40 -0400

    finished numpy

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -2,6 +2,7 @@ from solution import SOLUTION
 import constants as c
 import copy
 import os
+import numpy as np
 
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
@@ -14,13 +15,21 @@ class PARALLEL_HILL_CLIMBER:
         for key in range(0, c.populationSize):
             self.parents[key] = SOLUTION(self.nextAvailableID)
             self.nextAvailableID += 1
+        
+        self.currentGeneration = 0
+
+        self.ABTestArray = np.zeros((0,0)) # might brake
+
+
+        
 
     def Evolve(self):
 
         self.Evaluate(self.parents)
 
-        for currentGeneration in range(c.populationSize):
+        for self.currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
+
 
     def Evolve_For_One_Generation(self):
 
@@ -50,8 +59,9 @@ class PARALLEL_HILL_CLIMBER:
     def Print(self):
         print(" ")
         for key in range(c.populationSize):
-            print('Parent fitness: ', self.parents[key].fitness, ', Child fitness:', self.children[key].fitness)
-        print(" ")
+            pass
+            #print('Parent fitness: ', self.parents[key].fitness, ', Child fitness:', self.children[key].fitness)
+        #print(" ")
 
     def Show_Best(self):
 
@@ -68,4 +78,10 @@ class PARALLEL_HILL_CLIMBER:
 
         for key in range(0, c.populationSize):
             solutions[key].Wait_For_Simulation_To_End()
+            self.ABTestArray = np.insert(self.ABTestArray, self.currentGeneration, solutions[key].fitness) 
+        
+        print(self.ABTestArray)
+
+
+        
         
